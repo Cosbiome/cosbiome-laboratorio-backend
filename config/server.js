2431913d653a65979d69b1cfcaf7181b3d96fd7c
Cosbiome <cosbiomedev@gmail.com>
2021-02-05 17:01:13 -0600

    asdasd

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f59389776712c38817be7ac8d0d7578e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b3d54525-e10d-4eb6-99db-994b95e2b51e'
+};
